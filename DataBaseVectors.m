808c18ec8f0a6f029b740272162980d494dcc6dc
nicolasigor <n.tapia.in@gmail.com>
2016-12-15 02:30:04 -0300

    Add files via upload

---
+++ DataBaseVectors.m
@@ -0,0 +1,76 @@
+close all
+clear all
+%filename = ['CarasHombres/hombre (5).bmp'];
+%J = imread(filename);
+%[LBPImage, feats] = lbpFeatureExtractor(J, 7, 1, 0);%% hacer matriz con 1
+%figure, imshow(LBPImage, 'DisplayRange',[0,59]);
+%figure, bar(feats);
+%DataBaseCreator('BaseDeDatos');
+
+%ejecutable para hombres
+
+strh = strcat('BaseDeDatos/CarasHombres/*.bmp');
+strm = strcat('BaseDeDatos/CarasMujeres/*.bmp');
+imagefilesH = dir(strh);
+imagefilesM = dir(strm);
+nfilesH = length(imagefilesH);
+nfilesM = length(imagefilesM);
+%mkdir('BaseDeDatos','CarasHombres');
+%mkdir('BaseDeDatos','CarasMujeres');
+currentfilename = ['BaseDeDatos/CarasHombres/hombre (1).bmp'];
+ currentimage = imread(currentfilename);
+ [~, feats] = lbpFeatureExtractor(currentimage, 1, 1,0);
+ Raw=zeros(nfilesH+nfilesM,length(feats)+1);
+ %R(:,1)=1;
+ Raw(1,2:end)=feats(1:end);
+for h=2:nfilesH   
+ currentfilename = ['BaseDeDatos/CarasHombres/hombre (' num2str(h) ').bmp'];
+ currentimage = imread(currentfilename);
+[~, feats] = lbpFeatureExtractor(currentimage, 1, 1,0);
+ Raw(h,2:end)=feats(1:end);
+ 
+end
+ %%
+  Raw(nfilesH+1:end,1)=1;  
+for m=1:nfilesM
+ currentfilename = ['BaseDeDatos/CarasMujeres/mujer (' num2str(m) ').bmp'];
+ currentimage = imread(currentfilename);
+ [~, feats] = lbpFeatureExtractor(currentimage, 1, 1,0);
+ Raw(h+m,2:end)=feats(1:end);
+end
+
+%%
+
+csvwrite('LBPsinTruncarRaw.csv',Raw);
+
+%%
+
+currentfilename = ['BaseDeDatos/CarasHombres/hombre (1).bmp'];
+ currentimage = imread(currentfilename);
+ 
+ [~, feats] = lbpFeatureExtractor(currentimage, 1, 1,1);
+ R=zeros(nfilesH+nfilesM,length(feats)+1);
+ %R(:,1)=1;
+ R(1,2:end)=feats(1:end);
+ 
+for h=2:nfilesH   
+ currentfilename = ['BaseDeDatos/CarasHombres/hombre (' num2str(h) ').bmp'];
+ currentimage = imread(currentfilename);
+ [~, feats] = lbpFeatureExtractor(currentimage, 1, 1,1);
+ R(h,2:end)=feats(1:end);
+ 
+end
+ %%
+ R(nfilesH+1:end,1)=1; 
+for m=1:nfilesM
+ currentfilename = ['BaseDeDatos/CarasMujeres/mujer (' num2str(m) ').bmp'];
+ currentimage = imread(currentfilename);
+ [~, feats] = lbpFeatureExtractor(currentimage, 1, 1,1);
+ R(h+m,2:end)=feats(1:end);
+end
+
+%%
+
+csvwrite('LBPTruncadoRaw1.csv',R);
+%%
+coef = pca(R,'corr');
